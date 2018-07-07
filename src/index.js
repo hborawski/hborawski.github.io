317bf696c4268c847c8fcd1a1da40e69e13f0dec
Harris <harrisborawski@gmail.com>
2018-07-07 11:57:38 -0700

    built in react for overkill purposes

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/app.css'
+import App from './components/app'
+
+ReactDOM.render(<App/>, document.querySelector('#main'))
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpe?g)$/,
+        loaders: [
+          {
+            loader: 'lqip-loader',
+            options: {
+              base64: true,
+              palette: false
+            }
+          },
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8000
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+}
