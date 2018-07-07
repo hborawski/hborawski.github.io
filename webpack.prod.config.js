40b8ee1dc1fb2d606c24a07c954016b7d2e4cf0c
Harris <harrisborawski@gmail.com>
2018-07-07 12:11:12 -0700

    use multiple webpack configs so bundling uses publicPath

---
+++ webpack.prod.config.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: 'dist/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpe?g)$/,
+        loaders: [
+          {
+            loader: 'lqip-loader',
+            options: {
+              base64: true,
+              palette: false
+            }
+          },
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8000
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  plugins: [
+    new CleanWebpackPlugin([
+      'dist'
+    ])
+  ]
+}
